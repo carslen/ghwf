89823fc1b80848e0ea73999ce0367bbdd6081914
Carsten Lenz <git@lenz-online.org>
2024-10-23 14:41:50 +0200

    chore: remove exit

--- scripts/failure.sh
+++ scripts/failure.sh
@@ -6,5 +6,5 @@ echo "This script will fail"
 images=("alpine" "gibtsnicht:1.23.34")
 
 for image in "${images[@]}"; do
-    error=$(docker pull "$image" 2>&1) || echo -e "::err Failed to pull image: $image\n\nDetailed error message:\n$error" >> "$GITHUB_STEP_SUMMARY"
+    error=$(docker pull "$image" 2>&1) || echo -e "Failed to pull image: $image\n\nDetailed error message:\n$error" >> "$GITHUB_STEP_SUMMARY"; exit 1
 done
