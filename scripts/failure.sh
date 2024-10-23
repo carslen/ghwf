e162f38905b4cd836eecca7cd5b7c16c055068d6
Carsten Lenz <git@lenz-online.org>
2024-10-23 14:21:53 +0200

    chore: add exit 1 to fail workflow

--- scripts/failure.sh
+++ scripts/failure.sh
@@ -7,6 +7,5 @@ images=("alpine" "gibtsnicht:1.23.34")
 
 for image in "${images[@]}"; do
     error=$(docker pull "$image" 2>&1) ||
-    echo -e "Failed to pull $image\n\nDetailed error message:\n$error" >> "$GITHUB_STEP_SUMMARY" &&
-    exit 1
+    echo -e "Failed to pull $image\n\nDetailed error message:\n$error" >> "$GITHUB_STEP_SUMMARY"; exit 1
 done
