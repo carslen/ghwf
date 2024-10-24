844021bd6a8503ad8f3c6bbc70acdf4dd961a7dd
Carsten Lenz <git@lenz-online.org>
2024-10-24 10:32:27 +0200

    chore: code tags

--- scripts/failure.sh
+++ scripts/failure.sh
@@ -6,11 +6,10 @@ echo "This script will fail"
 images=("alpine" "gibtsnicht:1.12.123")
 
 for image in "${images[@]}"; do
-  echo "Pull $image"
-  error=$(docker pull "$image" 2>&1) || #echo -e "Failed to pull image: $image\n\nDetailed error message:\n$error" >>"$GITHUB_STEP_SUMMARY"
+  error=$(docker pull "$image" 2>&1) || echo -e "Failed to pull image: $image\n\nDetailed error message:\n<code>$error</code>" >>"$GITHUB_STEP_SUMMARY"
 
-  if [ "${PIPESTATUS[0]}" != "0" ]; then
-    echo -e "Failed to pull image: $image\n\nDetailed error message:\n$error" >>"$GITHUB_STEP_SUMMARY"
-    exit 1
-  fi
+#  if [ "${PIPESTATUS[0]}" != "0" ]; then
+#    echo -e "Failed to pull image: $image\n\nDetailed error message:\n$error" >>"$GITHUB_STEP_SUMMARY"
+#    exit 1
+#  fi
 done
